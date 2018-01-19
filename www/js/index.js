0a565fb724285c9e9900a549595917ca06927dff
Trevor <txrev91@gmail.com>
2018-01-19 16:58:25 -0500

    updated index.js with quite a lot of changes. the ones worth mentioning are: 1) removed the steps from the dialog boxes. It felt weird to have 2 steps up there for such a short form. 2) introduced a new function, verifyResults, that takes the output of the SweetAlert dialog boxes and runs through some quick validation to ensure that a) the users name does not have special characters, and b) the initiative is a positive number. 3) added throwError function. This function takes certain contexts passed to it and throws errors based on what happened. 4) updated the functions that add, create, edit, and delete player cards to run through the verifyResults function before adding anything to the screen.

--- www/js/index.js
+++ www/js/index.js
@@ -17,193 +17,231 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-        //setTimeout( this.testData,500 );
-    },
+  // Application Constructor
+  initialize: function() {
+      this.bindEvents();
+      //setTimeout( this.testData,500 );
+  },
 
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        $(document).on('deviceready',this.onDeviceReady);
-        $('nav').on('click','.btn-add-player',this.addPlayer);
-        $('#cardContainer').on('click','i.cancel-btn',this.removePlayer);
-        $('#cardContainer').on('click','i.edit-btn',this.editPlayer);
-    },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+      $(document).on('deviceready',this.onDeviceReady);
+      $('nav').on('click','.btn-add-player',this.addPlayer);
+      $('#cardContainer').on('click','i.cancel-btn',this.removePlayer);
+      $('#cardContainer').on('click','i.edit-btn',this.editPlayer);
+  },
 
-    testData: function(){
-      var result = {
-        value: ['Trevor','10','5']
-      };
+  testData: function(){
+    var result = {
+      value: ['Trevor','10','5']
+    };
 
-      app.createPlayerCard(result);
+    app.createPlayerCard(result);
 
-      var result2 = {
-        value: ['Kevin','15','5']
-      };
+    var result2 = {
+      value: ['Kevin','15','5']
+    };
 
-      app.createPlayerCard(result2);
+    app.createPlayerCard(result2);
 
-      var result3 = {
-        value: ['Matt','20','5']
-      };
+    var result3 = {
+      value: ['Matt','20','5']
+    };
 
-      app.createPlayerCard(result3);
-    },
+    app.createPlayerCard(result3);
+  },
 
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
 
-    },
+  },
 
-    addPlayer: function(){
-      swal.setDefaults({
+  addPlayer: function(){
+
+    var steps = [
+      {
+        title: 'Player or Creature Name:',
         input: 'text',
-        confirmButtonText: 'Next &rarr;',
-        showCancelButton: true,
-        progressSteps: ['1', '2']
-      });
-
-      var steps = [
-        {
-          title: 'Player or Creature Name:',
-          input: 'text',
-          focusCancel: true,
-          onOpen: () => {
-            app.preventFocus()
-          }
-        },
-        {
-          title: 'Initiative Roll:',
-          input: 'number',
-          focusCancel: true,
-          onOpen: () => {
-            app.preventFocus()
-          }
+        focusCancel: true,
+        onOpen: () => {
+          app.preventFocus()
         }
-      ];
+      },
+      {
+        title: 'Initiative Roll:',
+        input: 'number',
+        focusCancel: true,
+        onOpen: () => {
+          app.preventFocus()
+        }
+      }
+    ];
 
-      swal.queue(steps).then((result) => {
-        swal.resetDefaults();
-        app.createPlayerCard(result);
-      });
-    },
+    swal.queue(steps).then((result) => {
+      if(!result.dismiss){
+        var context = 'addPlayer';
+        var card = 'null';
+        app.verifyResults(card,result,context);
+      }
+    });
+  },
 
-    preventFocus: function(){
-      $('.swal2-container .swal2-modal').focus();
-    },
+  verifyResults: function(card,result,context){
+      var alphaNumeric = new RegExp("^[a-zA-Z0-9]+$");
+      var numeric = new RegExp("^[0-9]*$");
+      var nameCheck = false;
+      var iniCheck = false;
 
-    createPlayerCard: function(result){
+      if(alphaNumeric.test(result.value[0])){
+        nameCheck = true;
+      }
 
-      var playerCard = '<div class="col s12 m6">\
-          <div class="card blue-grey darken-1">\
-            <div class="card-content white-text">\
-              <div class="card-content-info">\
-                <span class="card-title">'+result.value[0]+'</span>\
-                <p>Initiative: <span class="totalIni">'+parseInt(result.value[1])+'</span></p>\
-              </div>\
-              <div class="card-content-controls">\
-                <i class="material-icons edit-btn">edit</i>\
-                <i class="material-icons cancel-btn">cancel</i>\
-              </div>\
+      if(numeric.test(result.value[1])){
+        iniCheck = true;
+      }
+
+      if(!nameCheck){
+       app.throwError(error='name'); 
+      }
+
+      if(nameCheck && !iniCheck){
+        app.throwError(error='initiatve');
+      }
+
+      if(nameCheck && iniCheck){
+        if(context == 'addPlayer'){
+          app.createPlayerCard(result);
+        }else if(context == 'editPlayer'){
+          app.updatePlayerInfo(card,result);
+        }
+      }
+  },
+
+  preventFocus: function(){
+    $('.swal2-container .swal2-modal').focus();
+  },
+
+  createPlayerCard: function(result){
+
+    var playerCard = '<div class="col s12 m6">\
+        <div class="card blue-grey darken-1">\
+          <div class="card-content white-text">\
+            <div class="card-content-info">\
+              <span class="card-title">'+result.value[0]+'</span>\
+              <p>Initiative: <span class="totalIni">'+parseInt(result.value[1])+'</span></p>\
+            </div>\
+            <div class="card-content-controls">\
+              <i class="material-icons edit-btn">edit</i>\
+              <i class="material-icons cancel-btn">cancel</i>\
             </div>\
           </div>\
-        </div>';
+        </div>\
+      </div>';
 
-      var $div = $("<div>", {'class': 'row hidden','data-initiative': parseInt(result.value[1])});
-      $div.append(playerCard);
+    var $div = $("<div>", {'class': 'row hidden','data-initiative': parseInt(result.value[1])});
+    $div.append(playerCard);
 
-      app.addCard($div);
-    },
+    app.addCard($div);
+  },
 
-    shuffleCards: function(){
-      var cardsToShuffle = $('.row');
-      if(cardsToShuffle.length > 1){
-        var cards = [];
-        for (var i = 0; i < cardsToShuffle.length; i++){
-          cards.push(cardsToShuffle[i]);
-        }
-        cards.sort(function(a,b){
-          return b.getAttribute('data-initiative') - a.getAttribute('data-initiative');
-        });
-
-        var container = $('#cardContainer');
-        container.empty();
-
-        cards.forEach(function(el){
-          container.append(el);
-        });
-
+  shuffleCards: function(){
+    var cardsToShuffle = $('.row');
+    if(cardsToShuffle.length > 1){
+      var cards = [];
+      for (var i = 0; i < cardsToShuffle.length; i++){
+        cards.push(cardsToShuffle[i]);
       }
-    },
+      cards.sort(function(a,b){
+        return b.getAttribute('data-initiative') - a.getAttribute('data-initiative');
+      });
 
-    addCard: function(elementToAdd){
       var container = $('#cardContainer');
-      container.append(elementToAdd);
-      elementToAdd.slideDown(500).removeClass('hidden');
-      app.shuffleCards();
-    },
+      container.empty();
 
-    removePlayer: function(){
-      var playerCard = $(this).closest('.row');
-      playerCard.slideUp(500, function(){
-        $(this).remove();
+      cards.forEach(function(el){
+        container.append(el);
       });
-    },
 
-    editPlayer: function(){
-      var card = $(this).closest('.row');
-      var cardContentInfo = card.find('.card-content-info');
-      var player = cardContentInfo.find('.card-title').text();
-      var initiative = cardContentInfo.find('.totalIni').text();
+    }
+  },
 
-      swal.setDefaults({
+  addCard: function(elementToAdd){
+    var container = $('#cardContainer');
+    container.append(elementToAdd);
+    elementToAdd.slideDown(500).removeClass('hidden');
+    app.shuffleCards();
+  },
+
+  removePlayer: function(){
+    var playerCard = $(this).closest('.row');
+    playerCard.slideUp(500, function(){
+      $(this).remove();
+    });
+  },
+
+  editPlayer: function(){
+    var card = $(this).closest('.row');
+    var cardContentInfo = card.find('.card-content-info');
+    var player = cardContentInfo.find('.card-title').text();
+    var initiative = cardContentInfo.find('.totalIni').text();
+
+    var steps = [
+      {
+        title: 'Player or Creature Name:',
         input: 'text',
-        confirmButtonText: 'Next &rarr;',
-        showCancelButton: true,
-        progressSteps: ['1', '2']
-      });
-
-      var steps = [
-        {
-          title: 'Player or Creature Name:',
-          input: 'text',
-          inputValue: player,
-          focusConfirm: 'true',
-          onOpen: () => {
-            app.preventFocus()
-          }
-        },
-        {
-          title: 'Initiative Roll:',
-          input: 'number',
-          inputValue: initiative,
-          focusConfirm: 'true',
-          onOpen: () => {
-            app.preventFocus()
-          }
+        inputValue: player,
+        focusConfirm: 'true',
+        onOpen: () => {
+          app.preventFocus()
         }
-      ];
+      },
+      {
+        title: 'Initiative Roll:',
+        input: 'number',
+        inputValue: initiative,
+        focusConfirm: 'true',
+        onOpen: () => {
+          app.preventFocus()
+        }
+      }
+    ];
 
-      swal.queue(steps).then((result) => {
-        swal.resetDefaults();
-        app.updatePlayerInfo(card,result);
-      });
-    },
+    swal.queue(steps).then((result) => {
+      swal.resetDefaults();
+      var context = 'editPlayer';
+      app.verifyResults(card,result,context);
+    });
+  },
 
-    updatePlayerInfo: function(card,result){
-      var totalIni = parseInt(result.value[1]);
-      var cardContent = card.find('.card-content-info');
-      card.attr('data-initiative',totalIni);
-      cardContent.find('.card-title').text(result.value[0]);
-      cardContent.find('.totalIni').text(totalIni);
+  updatePlayerInfo: function(card,result){
+    var totalIni = parseInt(result.value[1]);
+    var cardContent = card.find('.card-content-info');
+    card.attr('data-initiative',totalIni);
+    cardContent.find('.card-title').text(result.value[0]);
+    cardContent.find('.totalIni').text(totalIni);
 
-      app.shuffleCards();
-    },
+    app.shuffleCards();
+  },
+
+  throwError: function(error){
+    if(error == 'initiatve'){
+      swal(
+        'Oops...',
+        'Looks like the players initiative is incorrect. Please try adding or editing the player again.',
+        'error'
+      );
+    }else if(error == 'name'){
+      swal(
+        'Oops...',
+        'Looks like the players name is incorrect. Please try adding or editing the player again.',
+        'error'
+      );
+    }
+  }
 };
